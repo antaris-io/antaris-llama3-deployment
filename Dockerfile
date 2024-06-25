a2774ab5244ac5b574dc2e36aa5bf3dae03ec5d5
Janik Kreit <janikkreit@outlook.de>
2024-06-25 16:14:10 +0200

    bug fixes

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,17 @@
 FROM python:3.9-slim
+FROM nvidia/cuda:12.1.0-cudnn8-devel-ubuntu20.04
 
-# update and install build-essential
+# Install Python and necessary dependencies
 RUN apt-get update && \
-    apt-get install -y --no-install-recommends build-essential && \
-    apt-get clean && \
+    apt-get install -y --no-install-recommends \
+    python3.9 \
+    python3-pip \
+    build-essential \
+    wget && \
     rm -rf /var/lib/apt/lists/*
+
+# Set Python 3.8 as the default
+RUN update-alternatives --install /usr/bin/python python /usr/bin/python3.9 1
 
 # Create a directory for the app
 WORKDIR /opt/ml
--- create-endpoint.sh
+++ create-endpoint.sh
@@ -1,7 +1,5 @@
 # doc: https://docs.aws.amazon.com/sagemaker/latest/dg/serverless-endpoints-create.html
 
-# create a ECR repository and follow the instructions to push the image to the repository
-
 # upload model checkpoint to S3
 #aws s3 cp Meta-Llama-3-8B-hf/ s3://253333439226-app-registry/llama3/Meta-Llama-3-8B-hf/ --recursive
 #aws s3 cp Meta-Llama-3-8B-hf-finetuned/ s3://253333439226-app-registry/llama3/Meta-Llama-3-8B-hf-finetuned/ --recursive
@@ -10,14 +8,57 @@
 sudo docker login -u AWS -p $(aws ecr get-login-password --region eu-central-1) 253333439226.dkr.ecr.eu-central-1.amazonaws.com
 
 # create docker image
+echo "Building docker image"
 sudo docker build -t llama3-deployment .
 
 # tag docker image
 sudo docker tag llama3-deployment:latest 253333439226.dkr.ecr.eu-central-1.amazonaws.com/llama3-deployment:latest
 
+# delete old docker image on ECR
+echo "Deleting old docker image"
+sudo docker rmi 253333439226.dkr.ecr.eu-central-1.amazonaws.com/llama3-deployment:latest
+
 # push docker image
+echo "Pushing docker image"
 sudo docker push 253333439226.dkr.ecr.eu-central-1.amazonaws.com/llama3-deployment:latest
 
-# create a model (using the console)
-# create an endpoint configuration (using the console), instancwe type: ml.g4dn.12xlarge
-# create an endpoint (using the console)
+
+# remove old model
+echo "Deleting old model"
+aws sagemaker delete-model \
+    --model-name llama3-html-to-json \
+    --region eu-central-1
+
+# remove old endpoint configuration
+echo "Deleting old endpoint configuration"
+aws sagemaker delete-endpoint-config \
+    --endpoint-config-name llama3-html-to-json \
+    --region eu-central-1
+
+# remove old endpoint
+echo "Deleting old endpoint"
+aws sagemaker delete-endpoint \
+    --endpoint-name llama3-html-to-json \
+    --region eu-central-1
+
+# create a mode
+echo "Creating model"
+aws sagemaker create-model \
+    --model-name llama3-html-to-json \
+    --primary-container Image=253333439226.dkr.ecr.eu-central-1.amazonaws.com/llama3-deployment:latest \
+    --execution-role-arn arn:aws:iam::253333439226:role/service-role/SageMaker-ExecutionRole-20240219T160673 \
+    --region eu-central-1
+
+# create an endpoint configuration
+echo "Creating endpoint configuration"
+aws sagemaker create-endpoint-config \
+    --endpoint-config-name llama3-html-to-json \
+    --production-variants VariantName=AllTraffic,ModelName=llama3-html-to-json,InitialInstanceCount=1,InstanceType=ml.g4dn.12xlarge,InitialVariantWeight=1.0 \
+    --region eu-central-1
+
+# create an endpoint
+echo "Creating endpoint"
+aws sagemaker create-endpoint \
+    --endpoint-name llama3-html-to-json \
+    --endpoint-config-name llama3-html-to-json \
+    --region eu-central-1
